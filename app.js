656fdf87c81b877f3ef55488ba600e913d39ff76
Станислав <stdashkov@mail.ru>
2017-10-02 23:12:13 +0300

    add random number

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var scores, roundScore, activePlayer, dice;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-
 dice = Math.floor(Math.random() * 6) + 1;
 
-
 document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-1').textContent;

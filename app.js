1b5b88189bf292c31101c7851774fbde79edd759
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-05 21:12:05 +0300

    add new game function();

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 
 
@@ -78,12 +68,31 @@ function nextPlayer() {
 }
 
 
-document.querySelector('.btn-new').addEventListener('click', function() {
-  scores = [0,0];
-  activePlayer = 0;
-  roundScore = 0;
-})
+document.querySelector('.btn-new').addEventListener('click', init);
 
+function init() {
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = "Player 1!"
+  document.getElementById('name-1').textContent = "Player 2!"
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+
+
+
+
+}
 
 
 //

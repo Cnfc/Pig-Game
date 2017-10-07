698fb4b8e0ddbfbf1029f6c3e92f143262bf7334
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-07 12:58:48 +0300

    challenge 2 complete

--- challengers.js
+++ challengers.js
@@ -44,8 +44,19 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    var input = document.querySelector('.final-score').value;
+    var winningScore;
+    // console.log(input);
+    // undefined, 0, null, ""
+    if(input) {
+      winningScore = input;
+
+    } else {
+      winningScore = 100;
+    }
+
     // Check if player won the game
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = "Winner!"
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');

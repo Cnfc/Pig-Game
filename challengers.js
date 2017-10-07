d3dec48aad47ad6441b557a1612f49c37b4d6268
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-07 13:11:13 +0300

    complete the last challenge

--- challengers.js
+++ challengers.js
@@ -9,29 +9,43 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   if (gamePlaying) {
 
     //1. Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
     //2. Display the result
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + dice + '.png';
+    // var diceDom = document.querySelector('.dice');
+    // diceDom.style.display = 'block';
+    // diceDom.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = "block";
+    document.getElementById('dice-2').style.display = "block";
+    document.getElementById('dice-1').src = "dice-" + dice1 + ".png";
+    document.getElementById('dice-1').src = "dice-" + dice2 + ".png";
 
     //3. Update the round score
-    if(dice === 6 && lastDice === 6) {
-      //player looses score
-      scores[activePlayer] = 0;
-      document.querySelector('#score-' + activePlayer).textContent = '0';
-      nextPlayer();
-    } else if (dice !== 1 ) {
+    if (dice1 !== 1 && dice2 !==1) {
       //Add score
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
     } else {
       nextPlayer();
     }
 
-    lastDice = dice;
+    // if(dice === 6 && lastDice === 6) {
+    //   //player looses score
+    //   scores[activePlayer] = 0;
+    //   document.querySelector('#score-' + activePlayer).textContent = '0';
+    //   nextPlayer();
+    // } else if (dice !== 1 ) {
+    //   //Add score
+    //   roundScore += dice;
+    //   document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    //
+    // } else {
+    //   nextPlayer();
+    // }
+    //
+    // lastDice = dice;
   }
 });
 
@@ -58,7 +72,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Check if player won the game
     if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = "Winner!"
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice-1').style.display = "none";
+      document.getElementById('dice-2').style.display = "none";
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       gamePlaying = false;
@@ -84,8 +99,8 @@ function nextPlayer() {
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 
-  document.querySelector('.dice').style.display = 'none';
-
+  document.getElementById('dice-1').style.display = "none";
+  document.getElementById('dice-2').style.display = "none";
 }
 
 
@@ -97,7 +112,8 @@ function init() {
   activePlayer = 0;
   gamePlaying = true;
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = "none";
+  document.getElementById('dice-2').style.display = "none";
 
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';

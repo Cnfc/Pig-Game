7a6c742b459d28038f31b845154900bbcd477ef2
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-26 18:50:39 +0300

    hold & dice

--- challengers.js
+++ challengers.js
@@ -30,35 +30,53 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
 
   } else {
-    // NEXT PLAYER
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-
-    document.querySelector('.dice').style.display = 'none';
-
-
+    //NEXT PLAYER
+    nextPlayer();
   }
 
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // ADD CURRENT SCORE TO GLOBAL SCORE
+    scores[activePlayer] += roundScore;
 
+    // UPDATE THE UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if palyer winner
+    if(scores[activePlayer] >= 20 ) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    } else {
+      //NEXT PLAYER
+      nextPlayer();
+    }
+});
 
 
 // document.querySelector("#current-" + activePlayer).innerHTML = '<em>' + dice + "</em>"
 
 
 
+function nextPlayer() {
+  // NEXT PLAYER
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
 
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
 
 
 

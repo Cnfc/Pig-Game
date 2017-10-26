6c058c0794fc4fabe4e1ee2612599314d2b456e7
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-26 17:19:55 +0300

    clean score & corrent

--- challengers.js
+++ challengers.js
@@ -1,19 +1,58 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores =[0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 )+ 1;
-console.log(dice);
-
-document.querySelector("#current-" + activePlayer).textContent = dice;
-
-document.querySelector("#current-" + activePlayer).innerHTML = '<em>' + dice + "</em>"
 
 
 
 
-var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('#dice-2').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('corrent-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+// RANDOM NUMBER
+    var dice = Math.floor(Math.random() * 6 )+ 1;
+
+// DISPLAY THE RESULT
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+});
+
+
+
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).innerHTML = '<em>' + dice + "</em>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
